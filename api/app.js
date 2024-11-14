7436f90fda9b0787a18a83b921eb0ef01ac86bb5
Chamoli <godchamoli@gmail.com>
2024-11-14 18:44:17 +0530

    Configure CORS with specific options for enhanced security and functionality

--- api/app.js
+++ api/app.js
@@ -3,14 +3,24 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 const app = express();
-app.use(cors());
+
+// Configure CORS with options
+app.use(
+  cors({
+    origin: ['https://car-listing-ixro.onrender.com', 'http://localhost:5173'], 
+    credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 import userRoutes from './routes/user.routes.js';
-app.use('/api/user', userRoutes); 
+app.use('/api/user', userRoutes);
 
 import carRoutes from './routes/car.routes.js';
 app.use('/api/car', carRoutes);
